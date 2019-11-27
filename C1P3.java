67ab4a31664ca4ddb8539612da15178f48543c87
cg-libro1 <58238735+cg-libro1@users.noreply.github.com>
2019-11-27 12:49:05 -0600

    Add files via upload

---
+++ C1P3.java
@@ -0,0 +1,14 @@
+public class C1P3 {
+      public static void main(String[] args) {
+            double numero = 13.68;
+            System.out.println("Los colores, sabores y aromas marcan a México");
+            System.out.println("México tiene:" + "\t129 millones de habitantes");
+            System.out.println("Los tres países más poblados de América son:" + 
+                "\nEstados Unidos" + "\nBrasil" + "\nMéxico");
+            System.out.println("El número es: " + numero);
+            System.out.println("\"Ahora escribo entre comillas\"");
+            System.out.println("\'Ahora escribo entre apóstrofes\'");
+            System.out.println("Ahora ya no escribo más porque ya les expliqué y" +
+                 " no es el padre nuestro como para repetir 100 veces lo mismo");
+       }
+}
