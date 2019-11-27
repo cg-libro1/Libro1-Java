7322f0f691091e75f11b3308271fe40979e7d52e
cg-libro1 <58238735+cg-libro1@users.noreply.github.com>
2019-11-27 12:51:12 -0600

    Add files via upload

---
+++ C1P4.java
@@ -0,0 +1,11 @@
+public class C1P4 {
+      public static void main(String[] args) {
+            System.out.printf("%.2f %n", 12.37689456);
+            System.out.printf("\t%.4f \n", 12.37689456);
+            System.out.printf("%+.2f %n", 4.759865);
+            System.out.printf("%+d %n", 185);
+            System.out.printf("%8d %n", 185);
+            System.out.printf("%-7d %n", 185);
+            System.out.printf("%+-8d %n", 185);
+    }    
+}
---
+++ C1P5.java
@@ -0,0 +1,8 @@
+public class C1P5 {
+      public static void main(String[] args) {
+              int entero = 155;
+              double real = 3.789059;
+              System.out.printf("entero = %7d  \t real = %8.4f \n", entero, real);    
+      }
+}
+
